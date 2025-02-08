c5e611a5adb11110e61b888240f03aa8443b6b87
Sandip <jaiswarsandeep00@gmail.com>
2025-02-08 18:44:56 +0530

    github

--- 12.orm/core/models.py
+++ 12.orm/core/models.py
@@ -6,5 +6,5 @@ class Marvel(models.Model):
     last_name = models.CharField(max_length=50)
     heroic_name = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.heroic_name
+    # def __str__(self):
+    #     return self.heroic_name
--- 12.orm/core/views.py
+++ 12.orm/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from . models import Marvel
 # Create your views here.
 
 def index(request):
-    return render(request,'core/index.html')
+    mf = Marvel.objects.all()
+    return render(request,'core/index.html',{'mf':mf})
