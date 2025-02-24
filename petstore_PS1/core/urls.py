5d07a048b013da5ae7ae2046370aafabb218116f
Sandip <jaiswarsandeep00@gmail.com>
2025-02-24 20:53:44 +0530

    update

---
+++ petstore_PS1/core/urls.py
@@ -0,0 +1,21 @@
+# We are in urls.py file of core app
+
+from django.urls import path
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
+    path('dog_categories',views.dog_categories,name='dogcategories'),
+    path('cat_categories',views.cat_categories,name='catcategories'),
+    path('bird_categories',views.bird_categories,name='birdcategories'),
+    path('pet_details/<int:id>',views.pet_details,name='petdetails'),
+]
+
+#Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
