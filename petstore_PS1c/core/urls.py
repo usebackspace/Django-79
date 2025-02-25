c88370e30c650d49dffa9c2ec879c4cda11884e1
Sandip <jaiswarsandeep00@gmail.com>
2025-02-25 18:56:41 +0530

    update

--- petstore_PS1c/core/urls.py
+++ petstore_PS1c/core/urls.py
@@ -1,13 +1,10 @@
-
 # We are in urls.py file of core app
 
 from django.urls import path
 from . import views
 
-#------ To incude Media file ---------------
 from django.conf import settings
 from django.conf.urls.static import static
-#-----------------------------------------------
 
 urlpatterns = [
     path('',views.home,name='home'),
@@ -16,10 +13,9 @@ urlpatterns = [
     path('dog_categories',views.dog_categories,name='dogcategories'),
     path('cat_categories',views.cat_categories,name='catcategories'),
     path('bird_categories',views.bird_categories,name='birdcategories'),
-    path('pet_details/<int:id>/',views.pet_details,name='petdetails'),
+    path('pet_details/<int:id>',views.pet_details,name='petdetails'),
 ]
 
-
-#--------- THis is will add file to media folder -----------
+#Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
