aa6d22cb9ad57739979242eb5d287f0dab1d000e
Sandip <jaiswarsandeep00@gmail.com>
2025-02-28 21:02:08 +0530

    update

--- petstore_PS2/core/urls.py
+++ petstore_PS2/core/urls.py
@@ -1,10 +1,13 @@
+
 # We are in urls.py file of core app
 
 from django.urls import path
 from . import views
 
+#------ To incude Media file ---------------
 from django.conf import settings
 from django.conf.urls.static import static
+#-----------------------------------------------
 
 urlpatterns = [
     path('',views.home,name='home'),
@@ -13,9 +16,15 @@ urlpatterns = [
     path('dog_categories',views.dog_categories,name='dogcategories'),
     path('cat_categories',views.cat_categories,name='catcategories'),
     path('bird_categories',views.bird_categories,name='birdcategories'),
-    path('pet_details/<int:id>',views.pet_details,name='petdetails'),
+    path('pet_details/<int:id>/',views.pet_details,name='petdetails'),
+    path('registration/',views.registration,name='registration'),
+    path('login/',views.log_in,name='login'),
+    path('profile/',views.profile,name='profile'),
+    path('logout/',views.log_out, name="logout"),
+    path('changepassword/',views.changepassword, name="changepassword"),
 ]
 
-#Serve media files during development
+
+#--------- THis is will add file to media folder -----------
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
